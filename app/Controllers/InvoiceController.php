9fccd580839f95a39c91ef27e1e17f2d1e772be2
trung <trungnguyenhuynhminh46@gmail.com>
2023-09-27 19:50:52 +0700

    .

--- app/Controllers/InvoiceController.php
+++ app/Controllers/InvoiceController.php
@@ -5,8 +5,6 @@ declare(strict_types = 1);
 namespace App\Controllers;
 
 use App\Attributes\Get;
-use App\Enums\InvoiceStatus;
-use App\Models\Invoice;
 use Twig\Environment;
 
 class InvoiceController
@@ -21,16 +19,39 @@ class InvoiceController
     #[Get('/invoices')]
     public function index(): string
     {
-        $invoices = Invoice::query()
-            ->where('status', InvoiceStatus::Paid)
-            ->get()
-            ->map(fn(Invoice $invoice)=>[
-                'id' => $invoice->id,
-                'invoiceNumber' => $invoice->invoice_number,
-                'amount' => $invoice->amount,
-                'status' => $invoice->status->value,
-                'dueDate' => $invoice->due_date->toDateTimeString(),
-            ])->toArray();
+        // $invoices = Invoice::query()
+        //     ->where('status', InvoiceStatus::Paid)
+        //     ->get()
+        //     ->map(fn(Invoice $invoice)=>[
+        //         'id' => $invoice->id,
+        //         'invoiceNumber' => $invoice->invoice_number,
+        //         'amount' => $invoice->amount,
+        //         'status' => $invoice->status->value,
+        //         'dueDate' => $invoice->due_date->toDateTimeString(),
+        //     ])->toArray();
+        $invoices = [
+            [
+                'id' => 1,
+                'invoiceNumer' => 1,
+                'amount' => 1000,
+                'status' => 1,
+                'dueDate' => '2021-10-10 10:10:10',
+            ],
+            [
+                'id' => 2,
+                'invoiceNumer' => 1,
+                'amount' => 2000,
+                'status' => 1,
+                'dueDate' => '2021-10-10 10:10:10',
+            ],
+            [
+                'id' => 3,
+                'invoiceNumer' => 1,
+                'amount' => 3000,
+                'status' => 1,
+                'dueDate' => '2021-10-10 10:10:10',
+            ],
+        ];
         return $this->twig->render('invoices/index.twig', compact('invoices'));
     }
 }
